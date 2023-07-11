76f6f2db538151da0e6a809e0e090552ead6bc4f
Elgun <elgun.ezmemmedov@mail.ru>
2023-07-11 21:34:49 +0400

    'header'

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, } from 'react-router-dom';
 import Alert from './components/Alert'
 import { useDispatch } from 'react-redux';
 import './styles/global.scss';
+import Header from './components/Header/Header';
 function App() {
   const dispatch = useDispatch()
   useEffect(() => {
@@ -12,19 +13,26 @@ function App() {
   }, [dispatch])
 
   return (
-    <div className="App bg">
+    <div className="App ">
       <Router>
-        {/* <Alert /> */}
+        <Alert />
+        <Header />
+        <div className={"d"} style={{ height: "500px", background: "black", width: "100%", color: 'white', textTransform: 'capitalize', display: 'flex', alignItems: "center", justifyContent: 'center' }}>
 
+          <h1 style={{ color: 'white' }}>yakinda hizmetinizdeyiz</h1>
+        </div>
         {/* biz burani checked unchecked olunmasini dropdowndaki dark light mor ile degisirik Hansiki burdaki id="theme" ile ordaki label htmlFor "theme" eynidi Fuckk ! */}
         <div className={`App `}>
           <h1 className="text_white">
             Yakinda Hizmeyinizdeyizdsada
           </h1>
 
-          <Alert />
           {/* <Route exact path="/" component={auth?.token ? Home : Login} /> */}
+          {/* <PrivateRouter exact path="/:page/:id" component={PageRender} /> */}
+          {/*
+          <Route exact path="/register" component={Register} />
 
+*/}
         </div>
       </Router>
     </div>
