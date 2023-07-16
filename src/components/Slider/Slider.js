20b1ff6575bd893b0337421d27ea60656f58292f
Elgun <elgun.ezmemmedov@mail.ru>
2023-07-16 20:21:23 +0400

    's'

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,47 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Slider.scss";
 
-import { slideImages } from "../../constants/slideImages";
-import { AiOutlineLeft } from "react-icons/ai";
-import { AiOutlineRight } from "react-icons/ai";
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 
 const Slider = () => {
-  //https://www.youtube.com/watch?v=tphDji-XZCE
-  const [index, setIndex] = React.useState(0);
-  const [cars, setCars] = React.useState(slideImages);
-  useEffect(() => {
-    const lastIndex = cars.length - 1;
-    if (index < 0) setIndex(lastIndex);
-    if (index > lastIndex) setIndex(0);
-    let slider = setInterval(() => {
-      setIndex((oldIndex) => {
-        let index = oldIndex + 1;
-        if (index > cars.length - 1) index = 0;
-        return index;
-      });
-    }, 5000);
-    return () => {
-      clearInterval(slider);
-    };
-  }, [index, cars]);
-  const handleClick = (par) => {
-    console.log("das");
+  const [index, setIndex] = useState(0);
 
-  }
+
+
   return (
-    <div className={"slider_container"}>
-      <div className={"slider sliderrr"}>
-       
-        <div className={"navigation"}>
-          <AiOutlineLeft className={"prev_btn"} onClick={() => setIndex(index - 1)} />
-          <AiOutlineRight className={"next_btn"} onClick={() => setIndex(index + 1)} />
+    <div className="slider_container">
+      <div className="slider sliderrr">
+  
+
+        {/* Navigation buttons */}
+        <div className="navigation">
+          <AiOutlineLeft className="prev_btn" onClick={() => setIndex(index - 1)} />
+          <AiOutlineRight className="next_btn" onClick={() => setIndex(index + 1)} />
         </div>
 
-        {/* //!clickable navigations */}
-        <div className={"visibility_navigation"}>
+        {/* Clickable navigations */}
+        <div className="visibility_navigation">
           <div className={`slide_icon ${index === 0 ? "activeNavigationSlider" : ""}`} onClick={() => setIndex(0)}></div>
-          <div className={` slide_icon ${index === 1 ? "activeNavigationSlider" : ""}`} onClick={() => setIndex(1)}></div>
-          <div className={`slide_icon ${index === 2 ? "activeNavigationSlider" : ""}`} onClick={() => setIndex(2)} ></div>
+          <div className={`slide_icon ${index === 1 ? "activeNavigationSlider" : ""}`} onClick={() => setIndex(1)}></div>
+          <div className={`slide_icon ${index === 2 ? "activeNavigationSlider" : ""}`} onClick={() => setIndex(2)}></div>
         </div>
       </div>
     </div>
