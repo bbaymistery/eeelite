5d43284ed600ec2b0e381cfa08b581a1b1d23e76
Elgun <elgun.ezmemmedov@mail.ru>
2023-07-16 20:08:16 +0400

    's'

---
+++ src/constants/slideImages.js
@@ -0,0 +1,21 @@
+import Imagee from "../images/sliderImages/img1o_.jpg";
+import Imagee2 from "../images/sliderImages/img2o.jpg";
+import Imagee3 from "../images/sliderImages/img3o.jpg";
+export const slideImages = [
+  {
+    id: 1,
+    img: Imagee,
+  },
+
+  {
+    id: 2,
+    img: Imagee2,
+  },
+
+  {
+    id: 3,
+    img: Imagee3,
+  },
+];
+
+//for changing side bar active item  when we click to each item
