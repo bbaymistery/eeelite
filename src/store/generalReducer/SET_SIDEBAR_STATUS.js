f06886eb2172de8a15b8fd08bfd247b3247ed983
Elgun  Ezmemmedov <85512325+bbaymistery@users.noreply.github.com>
2023-07-16 20:07:45 +0400

    Delete src directory

--- src/store/generalReducer/SET_SIDEBAR_STATUS.js
+++
@@ -1,8 +0,0 @@
-function SET_SIDEBAR_STATUS(params = {}) {
-    let { state, } = params
-    // let { data: { name, value, } } = action
-    let newState = JSON.parse(JSON.stringify(state))
-
-    return newState;
-}
-export default SET_SIDEBAR_STATUS
