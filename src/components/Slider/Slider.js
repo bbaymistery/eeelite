bd4743246ceb1f0d7b5a98b58ea6d477a787aa63
Elgun <elgun.ezmemmedov@mail.ru>
2023-07-16 20:23:24 +0400

    's'

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,17 +1,72 @@
 import React, { useEffect, useState } from "react";
 import "./Slider.scss";
 
+import { slideImages } from "../../constants/slideImages";
 import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 
 const Slider = () => {
   const [index, setIndex] = useState(0);
+  const [cars, setCars] = useState(slideImages);
 
+  useEffect(() => {
+    const lastIndex = cars.length - 1;
 
+    // Ensure the index stays within the valid range
+    if (index < 0) setIndex(lastIndex);
+    if (index > lastIndex) setIndex(0);
+
+    // Auto slide functionality
+    let slider = setInterval(() => {
+      setIndex((oldIndex) => {
+        let newIndex = oldIndex + 1;
+        if (newIndex > lastIndex) newIndex = 0;
+        return newIndex;
+      });
+    }, 5000);
+
+    // Cleanup the interval when the component is unmounted
+    return () => {
+      clearInterval(slider);
+    };
+  }, [index, cars]);
 
   return (
     <div className="slider_container">
       <div className="slider sliderrr">
-  
+        {/* Render slides */}
+        {slideImages.map((image, carIndex) => {
+          let position = "nextSlide";
+          if (carIndex === index) position = "activeSlide";
+          if (carIndex === index - 1 || (index === 0 && carIndex === cars.length - 1))
+            position = "lastSlide";
+
+          return (
+            <div
+              className={`slide ${position} eachslider`}
+              key={image.id}
+              style={{ backgroundImage: `url(${image.img})` }}
+            >
+              {/* Slide content */}
+              <div className="container_auto_center">
+                <div className="intro">
+                  <h1 className="intro_welcome">Welcome to EE Elite</h1>
+                  <h2 className="intro_header">
+                    Discover Premium Products <br /> for Your Every Need
+                  </h2>
+                  <p className="intro_desc">
+                    Elevate your lifestyle with our exclusive selection of high-quality products,
+                    meticulously curated to provide you with unmatched comfort and satisfaction.
+                  </p>
+                  <div className="login_register">
+                    <a href="/products">
+                      <button className="login_register_button">Explore Our Collection</button>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
 
         {/* Navigation buttons */}
         <div className="navigation">
